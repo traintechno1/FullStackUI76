80ed2c6ac321c2b39e8713da04ca8c91b8e610fa
traintechno1 <admin@Chaitalis-MacBook-Air.local>
2024-10-24 07:47:12 +0530

    Arithmetic Operators

---
+++ JS/arithmeticOperators.js
@@ -0,0 +1,57 @@
+let a = 10;
+let b = 20;
+let result;
+
+// undefined + undefined; // NaN -> not a number
+// when a and b do not have assignments
+// result = a + b;  // NaN -> not a number
+
+result = a + b;
+console.log("Addition = " , result); // Addition -> 30
+
+result = a - b; // Substraction
+console.log("Substraction = " ,result); // -10
+
+result = a * b; // Multiplication
+console.log("Multiplication = " ,result); // 200
+
+result = a / b; // Division
+console.log("Division = " ,result); // 0.5
+
+result = a % b; // Modulus -> It gives remainder
+console.log("Modulus = " ,result); // 10
+
+//         15
+//        ---------------
+//    2 )   30
+//          2
+//         ----
+//          10
+//          10
+//         ----
+//          0
+
+
+//          11
+//        ---------------
+//    3 )   34
+//          3
+//         ----
+//          4
+//          3
+//         ----
+//          1 -> remainder
+
+
+
+
+
+
+
+
+
+
+
+
+
+
