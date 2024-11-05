277c10eb3b076d6722382a3b9dea870019d1ce9b
traintechno1 <admin@Chaitalis-Air.lan>
2024-11-05 07:30:06 +0530

    JS Logical Operators

---
+++ JS/logicalOperators.js
@@ -0,0 +1,27 @@
+
+let age = 20;
+
+let result = age > 0 && age < 18 
+
+// console.log(result);
+
+let value = 100;
+
+let result1 = value > 120 || value > 80;
+
+// console.log(result1);
+
+let a = false;
+
+let result2 = !a;
+
+// console.log(result2); 
+
+//age: number
+
+// age = 0;
+// age = "";
+// age = null;
+// age = undefined;
+
+console.log(!!age);
