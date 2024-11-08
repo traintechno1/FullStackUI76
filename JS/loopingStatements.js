35ad6fdc816b70c821e90755f89df931bce8f603
traintechno1 <admin@Chaitalis-MacBook-Air.local>
2024-11-08 07:02:52 +0530

    Loops in JS

---
+++ JS/loopingStatements.js
@@ -0,0 +1,18 @@
+
+// for(let i = 1; i <= 10; i++ ){
+//     console.log('hi');
+// }
+
+// console.log("Loop Ends:");
+
+let i = 11;
+
+while(i <= 10){
+    console.log("Hello", i);
+    i++;
+}
+
+do{
+    console.log("I am from do while");
+    i++;
+}while(i<=10);
