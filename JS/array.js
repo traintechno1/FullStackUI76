626d4cb3d1da5a33e15df1ae9345d6103e7d8fdc
traintechno1 <admin@Chaitalis-MacBook-Air.local>
2024-11-12 08:33:43 +0530

    JS Array and array methods

---
+++ JS/array.js
@@ -0,0 +1,111 @@
+let arr = [100, 200, 300, 400, 500, 600, 700];
+
+// arr[0] = 23432;
+// console.log(arr[0]); // 100
+// console.log(arr[1]); // 200
+// console.log(arr[2]); // 300
+// console.log(arr[3]); // 400
+// console.log(arr[4]); // 500
+// console.log(arr[5]); // 600
+// console.log(arr[6]); // 700
+
+// arr[7] = 800;
+// console.log(arr[7]);
+
+// arr[18] = 900;
+// console.log(arr[18]);
+
+// console.log(arr);
+
+// for(let i = 0; i < arr.length ; i++){
+//     console.log(arr[i]); // print array value
+// }
+
+// let arr1 = [1,2,3,4,5,6];
+// let arr3 = arr.concat(arr1);
+
+// console.log(arr.copyWithin(0, 3, 6));
+
+// let listOfArrayEntries = arr.entries();
+
+// for(let value of listOfArrayEntries ){
+//     console.log(value);
+// }
+
+// console.log(arr3);
+// console.log(arr);
+// console.log(arr1);
+
+// let result = arr.every(v=> v == 0);
+// console.log(result);
+
+
+// console.log(arr.fill(4, 0, 2));
+
+// let result = arr.filter(e=>e > 400);
+// console.log(result);
+
+// let result = arr.find((value)=> value === 300); 
+// console.log(result);
+
+// let index = arr.findIndex((v)=>{
+//    return v === 800
+// });
+
+// console.log(index);
+
+// console.log(arr);
+// console.log(arr.flat());
+
+// arr.forEach((v)=>{
+//     console.log(v)
+// })
+
+// console.log(arr.includes(300));
+
+// console.log(arr.indexOf(2000));
+
+// console.log(arr.join(','));
+
+// let keys =  arr.keys();
+
+// for(let key of keys){
+//     console.log(arr[key]);
+// }
+
+// arr.push(10000);
+// arr.push(20000);
+
+// arr.pop();
+// arr.pop();
+
+// let arr1 = arr.map((e)=> e / 2);
+// console.log(arr1);
+
+// let addition = arr.reduce((sum, num)=> sum + num , 0)
+// console.log(addition);
+
+// arr.reverse();
+// [100, 200, 300, 400, 500, 600, 700];
+// let firstElement =  arr.shift();
+// console.log(arr.shift()); // 100
+// console.log(arr.shift()); // 200
+// console.log(arr.shift()); // 300
+
+// console.log(arr); // [400, 500, 600, 700]
+
+// console.log(arr.slice(1, 4));
+// arr.push(1100);
+// console.log(arr.some((ele)=> ele > 1000));
+
+// arr.sort((num1, num2)=> num2 - num1);
+
+// arr.splice(1, 5);
+
+// console.log(arr.toString());
+
+// arr.unshift(800);
+
+console.log(arr);
+
+
