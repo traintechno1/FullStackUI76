90cbd7867e3c700164fe867654ae034306a9fe73
traintechno1 <admin@Chaitalis-MacBook-Air.local>
2024-11-14 08:16:45 +0530

    String in JS

---
+++ JS/string.js
@@ -0,0 +1,43 @@
+// let f_name = "Rutik";
+// let l_name = "Ambhore";
+
+// let full_name = `This is full name : ${f_name} ${l_name}`;
+
+// console.log(full_name);
+
+
+let message = "This is a test message";
+
+console.log(message);
+// console.log(message.charAt(5));
+// console.log(message.charCodeAt(5));
+// console.log(message.concat(' and now appending new one'));
+// console.log(message.endsWith('age'));
+// console.log(message.includes('si'));
+// console.log(message.indexOf('e'));
+// console.log(message.lastIndexOf('e'));
+// console.log(message.length);
+// console.log(message.match('[a-z]'));
+// console.log(message.padEnd(30, '-'));
+// console.log(message.padStart(30, '*'));
+
+// message.concat('\n');
+// message.concat(' appending something else');
+
+// console.log(message.repeat(5));
+
+// console.log(message.replace('is', 'en'));
+// console.log(message.search('st'));
+// console.log(message.slice(4, 9));
+
+// console.log(message.split(' '));
+
+// console.log(message.startsWith('Th'));
+// console.log(message.substring(2,16));
+// console.log(message.toLowerCase());
+// console.log(message.toUpperCase());
+// console.log(message.trim());
+// console.log(message.trimStart());
+// console.log(message.trimEnd());
+
+console.log(message);
